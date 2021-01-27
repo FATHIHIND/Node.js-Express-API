e816d8dd1cbb8b61c6c61768c58e7bf2eaa6063a
FATHIHIND <hind10bella.Fathi@gmail.com>
2021-01-27 20:33:09 +0100

    add router

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) =>
-     res.send(`Serveur node et express sur port ${PORT}`)
+routes(app);
+
+app.get('/', (req, res) => 
+    res.send(`Serveur node et express sur port ${PORT}`)
 );
 
 app.listen(PORT, () => 
-     console.log(`votre serveur est sur le port ${PORT}`)
-)
+    console.log(`votre serveur est sur le port ${PORT}`)
+);
